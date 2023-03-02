1d5823641e2251c0779ac3164cfe9d9518e2d775
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-03 00:16:32 +0200

    setup basic auth with two inmemory users

---
+++ src/main/java/com/asen/springsecuritybasicauth/config/UserConfig.java
@@ -0,0 +1,30 @@
+package com.asen.springsecuritybasicauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build());
+
+        manager.createUser(User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build());
+
+        return manager;
+    }
+
+}
